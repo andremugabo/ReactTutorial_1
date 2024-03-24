b8dc614f06915092e736906bec93ae071955a56d
andremugabo <andremugabo@yahoo.fr>
2024-03-25 00:29:48 +0200

    card css

--- myreactApp/src/App.jsx
+++ myreactApp/src/App.jsx
@@ -1,14 +1,17 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
+import Card from "./Card.jsx";
 function App() {
   return (
     <>
       <Header />
       <Food />
-      <Food />
-      <Food />
-      <Food />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Footer />
     </>
   );
--- myreactApp/src/Card.jsx
+++ myreactApp/src/Card.jsx
@@ -1,2 +1,11 @@
-function Card() {}
+import profilePic from "./assets/images.jpg";
+function Card() {
+  return (
+    <div className="card">
+      <img className="card-image" src={profilePic} alt="profile picture"></img>
+      <h2 className="card-title">Bro Code</h2>
+      <p className="card-text">I make youtube videos and play video games</p>
+    </div>
+  );
+}
 export default Card;
