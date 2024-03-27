9c00d1a480f817e530ff012c0ee3faab5cd778f9
MUGABO Andre <andremugabo@yahoo.fr>
2024-03-28 00:51:19 +0200

    list redering

--- myreactApp/src/App.jsx
+++ myreactApp/src/App.jsx
@@ -8,6 +8,20 @@ import Student from "./Student.jsx";
 import UserGreeting from "./UserGreeting.jsx";
 import List from "./List.jsx";
 function App() {
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "orange", calories: 45 },
+    { id: 3, name: "banana", calories: 129 },
+    { id: 4, name: "coconut", calories: 37 },
+    { id: 5, name: "pineapple", calories: 159 },
+  ];
+  const vegetables = [
+    { id: 6, name: "potatoes", calories: 110 },
+    { id: 7, name: "celery", calories: 45 },
+    { id: 8, name: "carrots", calories: 189 },
+    { id: 9, name: "corns", calories: 27 },
+    { id: 10, name: "broccoli", calories: 37 },
+  ];
   return (
     <>
       <Header />
@@ -25,7 +39,10 @@ function App() {
       <Student name="Sandy" age={27} isStudent={true} />
       <Student />
       <UserGreeting isLoggedIn={true} userName="BroCode" />
-      <List />
+      {fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
+      {vegetables.length > 0 ? (
+        <List items={vegetables} category="Vegetables" />
+      ) : null}
       <Footer />
     </>
   );
--- myreactApp/src/List.jsx
+++ myreactApp/src/List.jsx
@@ -1,28 +1,72 @@
 // rendering a list
-function List() {
+import propTypes from "prop-types";
+function List(props) {
   //   const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
   //   fruits.sort();
   //   return fruits;
   //   const listItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>);
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 19 },
-    { id: 4, name: "coconut", calories: 37 },
-    { id: 5, name: "pineapple", calories: 159 },
-  ];
+  //   const fruits = [
+  //     { id: 1, name: "apple", calories: 95 },
+  //     { id: 2, name: "orange", calories: 45 },
+  //     { id: 3, name: "banana", calories: 129 },
+  //     { id: 4, name: "coconut", calories: 37 },
+  //     { id: 5, name: "pineapple", calories: 159 },
+  //   ];
 
   //   fruits.sort((a, b) => a.name.localeCompare(b.name)); //ALPHABETICAL
   //   fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse alphabetical
-  fruits.sort((a, b) => a.calories - b.calories); //NUMERICAL ORDER
-  fruits.sort((a, b) => b.calories - a.calories); //reverse NUMERICAL ORDER
+  //   fruits.sort((a, b) => a.calories - b.calories); //NUMERICAL ORDER
+  //   fruits.sort((a, b) => b.calories - a.calories); //reverse NUMERICAL ORDER
 
-  const listItems = fruits.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name}&nbsp;<b>{fruit.calories}</b>
+  // const listItems = fruits.map((fruit) => (
+  //     <li key={fruit.id}>
+  //       {fruit.name}&nbsp;<b>{fruit.calories}</b>
+  //     </li>
+  //   ));
+
+  //   const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100);
+  //   const highCalFruits = fruits.filter((fruit) => fruit.calories > 100);
+  const category = props.category;
+  const itemList = props.items;
+
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}&nbsp;<b>{item.calories}</b>
     </li>
   ));
 
-  return <ul>{listItems}</ul>;
+  //   const listItems = lowCalFruits.map((lowCalFruit) => (
+  //     <li key={lowCalFruit.id}>
+  //       {lowCalFruits.name}&nbsp;{lowCalFruit.calories}
+  //     </li>
+  //   ));
+
+  //   const anotherList = highCalFruits.map((highCalFruit) => (
+  //     <li key={highCalFruit.id}>
+  //       {highCalFruit.name}&nbsp;{highCalFruit.calories}
+  //     </li>
+  //   ));
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ul className="list-items">{listItems}</ul>
+      {/* <ul>{anotherList}</ul> */}
+    </>
+  );
 }
+
+List.propTypes = {
+  category: propTypes.string,
+  items: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number,
+      name: propTypes.string,
+      calories: propTypes.number,
+    })
+  ),
+};
+List.defaultsProps = {
+  category: "Category",
+  items: [],
+};
 export default List;
